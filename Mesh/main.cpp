e0988e5bb60b41514bbb5374907324394c23e879
Gary <wgr54@126.com>
2016-01-12 19:56:18 +0800

    Initial Commit

---
+++ Mesh/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mesh
+//
+//  Created by Gary Wang on 16/1/12.
+//  Copyright © 2016年 Gary Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
